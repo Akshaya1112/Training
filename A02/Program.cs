9cfc3fddd9d9f771d700f44c6c35df047065c90f
akshaya <akshaya.duraisamy@trumpf.com>
2026-08-03 12:54:08 +0530

    Review Fix

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,32 +1,33 @@
-﻿namespace A02;
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2026.
+// Copyright (c) Metamation India.
+// -------------------------------------------------------------------------------------------------
+// Program.cs
+// Program to generate a random number and let the user guess it with hints.
+namespace A02;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-      var rd = new Random ();
-      int number = rd.Next (0, 101);
-      int guess;
-      int attempts = 0;
-      int low = 1;
-      int high = 100;
-      while (true) {
-         Console.Write("Enter your guess:");
-         while (!int.TryParse (Console.ReadLine (), out guess) && guess >= 1 && guess <= 100)
-            Console.WriteLine ("Enter number between 1 to 100");
+class Program {
+   static void Main () {
+      Random rd = new ();
+      int number = rd.Next (1, 101);
+      int guess, attempts = 0, low = 1, high = 100;
+      while (attempts < 7) {
+         Console.Write ("Enter your guess (1-100): ");
+         while (!int.TryParse (Console.ReadLine (), out guess) || guess < 1 || guess > 100)
+            Console.Write ("Enter a number between 1 and 100: ");
          attempts++;
          if (guess > number) {
-            //Console.WriteLine(" Guess Lower");
             high = guess - 1;
             Console.WriteLine ($"Your guess is high. Try guessing between {low} to {high}");
          } else if (guess < number) {
-            //Console.WriteLine("Guess Higher");
             low = guess + 1;
             Console.WriteLine ($"Your guess is low. Try guessing between {low} to {high}");
          } else {
-            Console.WriteLine ($"You guessed correctly. Attempts:{attempts}");
-            break;
+            Console.WriteLine ($"You guessed correctly. Attempts: {attempts}");
+            return;
          }
       }
+      Console.WriteLine ($"\nYou have used all 7 attempts.");
+      Console.WriteLine ($"The correct number was : {number}");
    }
 }
